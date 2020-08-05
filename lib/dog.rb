e69f439698d3e932bdace6706473f014603bf1f3
Eugene O'Neil <ejo7220@gmail.com>
2020-08-05 13:38:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog 
-  def initialize(breed = "Mutt")
+  def initialize(name, breed = "Mutt")
     @breed = breed
+    @name = name
   end
   
   attr_accessor :breed 
+  attr_accessor :name
   
   
 end
